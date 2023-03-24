b3241dc94682b5d675e3e3cd78c71f72ff62573f
therealjarbean <jarona@DESKTOP-C42JSTT.localdomain>
2023-03-23 20:51:34 -0400

    Add NodeJS and MySQL

---
+++ js/loginHandler.js
@@ -0,0 +1,16 @@
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+    host: "",
+    user: "",
+    password: "",
+})
+
+con.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+    con.query("", function (err, result) {
+        if (err) throw err;
+        console.log("Query did a thing");
+    });
+});
